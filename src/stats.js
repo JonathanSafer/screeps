a2314a4c358098c52d42cc05ca3a8f3e078adefa
jordansafer <jsafer@andrew.cmu.edu>
2019-06-05 00:04:37 -0400

    imports for stats

--- src/stats.js
+++ src/stats.js
@@ -1,3 +1,25 @@
+var rMe = require('medic');
+var rBM = require('bigMedic')
+var rTr = require('trooper')
+var rBT = require('bigTrooper')
+var rBB = require('bigBreaker')
+var rH = require('harasser');
+var rSB = require('spawnBuilder');
+var rC = require('claimer');
+var rE = require('eye');
+var rRo = require('robber');
+var rF = require('ferry');
+var rMM = require('mineralMiner');
+var rU = require('upgrader');
+var rB = require('builder');
+var rR = require('runner');
+var rBr = require('breaker');
+var rT = require('transporter');
+var rM = require('remoteMiner');
+var rS = require('scout');
+var rA = require('attacker');
+var u = require('utils');
+
 var stats = {
     collectStats: function() {
         //stats
