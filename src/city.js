67acc3962270b676ae1d2418e2acc1e8d4e44473
Jonathan Safer <jonathansafer2@gmail.com>
2019-09-17 10:39:42 -0400

    powerBank targeting adjustment

--- src/city.js
+++ src/city.js
@@ -700,7 +700,7 @@ function runObs(city){
             let powerBank = _.find(structures, structure => structure.structureType === STRUCTURE_POWER_BANK);
             let flagName = city + 'powerMine'
             if (powerBank && Game.cpu.bucket > 6000 && powerBank.ticksToDecay > 2500 && !Game.flags[flagName] &&
-                    Game.rooms[roomName].find(FIND_STRUCTURES).length < 10){
+                    Game.rooms[roomName].find(FIND_STRUCTURES).length < 30){
                 let walls = 0
                 let terrain = Game.rooms[roomName].getTerrain();
                 let x = powerBank.pos.x - 1
