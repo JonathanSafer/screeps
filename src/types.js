2ba9e697d8f8772ccb3c39c0030be5b2969acafb
Jonathan Safer <jonathansafer2@gmail.com>
2019-10-16 11:20:37 -0400

    body fix

--- src/types.js
+++ src/types.js
@@ -117,7 +117,7 @@ function getRecipe(type, extensions, room){
             d['bigTrooper'] = body([16, 24, 10], [TOUGH, RANGED_ATTACK, MOVE]);
             d['trooper'] = body([25, 25], [RANGED_ATTACK, MOVE]);
             d['robber'] = body([25, 25], [CARRY, MOVE]);
-            d['depositMiner'] = body([20, 4, 24], [WORK, CARRY, MOVE]);
+            d['depositMiner'] = body([20, 2, 22], [WORK, CARRY, MOVE]);
             break;
 	}
 
