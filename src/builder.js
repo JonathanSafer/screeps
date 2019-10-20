3c38d2b4da867073e374975d6eeb29a76ebc2d19
Jonathan Safer <jonathansafer2@gmail.com>
2019-10-20 15:02:55 -0400

    new builder priorities at RCL 8

--- src/builder.js
+++ src/builder.js
@@ -9,6 +9,19 @@ var rB = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        if(Game.spawns[creep.memory.city].room.controller.level === 8){//at RCL 8, only build, or repair ramparts and walls
+            rB.decideWhetherToBuild(creep);
+            if(creep.memory.building){
+                if(!rB.build(creep)){
+                    rB.repWalls(creep)
+                }
+            } else {
+                if(creep.withdraw(Game.spawns[creep.room.city].room.storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE){
+                    creep.moveTo(creep.moveTo(Game.spawns[creep.room.city].room.storage, {reusePath: 15, range: 1, swampCost: 2, plainCost: 2}))
+                }
+            }
+            return;
+        }
         rB.decideWhetherToBuild(creep);
         if (creep.memory.building) {
             a.build(creep);
@@ -28,6 +41,53 @@ var rB = {
             location = Game.spawns[creep.memory.city];
         }
         return location;
+    },
+
+    build: function(creep){
+        if(creep.memory.build){//check for site and build
+            let site = Game.getObjectById(creep.memory.build)
+            if(site){//if there is a build site, build it, else set build to null
+                //build site
+                if(creep.build(site) === ERR_NOT_IN_RANGE){
+                    creep.moveTo(site, {reusePath: 15, range: 3, swampCost: 2, plainCost: 2})
+                }
+                return true;
+            } else {
+                creep.memory.build = null
+            }
+        }
+        if(Game.time % 20 === 0){//occasionally scan for construction sites
+            var targets = Game.spawns[creep.memory.city].room.find(FIND_MY_CONSTRUCTION_SITES)
+            if(targets.length){
+                creep.memory.build = targets[0];
+                return true;
+            }
+        }
+        return false;
+    },
+
+    repWalls: function(creep){
+        if(creep.memory.repair){//check for target and repair
+            let target = Game.getObjectById(creep.memory.repair)
+            if(target){//if there is a target, repair it
+                if(creep.repair(target) === ERR_NOT_IN_RANGE){
+                    creep.moveTo(target, {reusePath: 15, range: 3, swampCost: 2, plainCost: 2})
+                }
+                return;
+            } else {
+                creep.memory.repair = null
+            }
+        }
+        if(Game.time % 200 === 0 || !creep.memory.repair){//occasionally scan for next target to repair
+            const buildings = Game.spawns[creep.memory.city].room.find(FIND_STRUCTURES)
+            const walls = _.filter(buildings, struct => struct.structureType === STRUCTURE_RAMPART || struct.structureType === STRUCTURE_WALL)
+            if(walls.length){//find lowest hits wall
+                sortedWalls = _.sortBy(walls, wall => wall.hits)
+                creep.memory.repair = sortedWalls[0]
+                return;
+            }
+        }
+        return;
     },
 
     decideWhetherToBuild: function(creep) {
