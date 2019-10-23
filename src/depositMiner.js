e68aa96e29a83a04b9503fa4a5c4b99f9b61b7d1
Jonathan Safer <jonathansafer2@gmail.com>
2019-10-23 13:13:08 -0400

    deposit miner should not do more than 1 trip

--- src/depositMiner.js
+++ src/depositMiner.js
@@ -9,7 +9,7 @@ var rDM = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (_.sum(creep.store) === 0 && creep.ticksToLive < 130){//if old and no store, suicide
+        if (_.sum(creep.store) === 0 && creep.ticksToLive < 500){//if old and no store, suicide
             creep.suicide()
             return;
         }
