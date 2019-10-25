902c09168f62bb0e79ad83c67e18314a7b1769d5
Jonathan Safer <jonathansafer2@gmail.com>
2019-10-25 13:40:32 -0400

    bug fix

--- src/ferry.js
+++ src/ferry.js
@@ -190,7 +190,7 @@ var rF = {
                     let result = creep.transfer(factory, creep.memory.mineral, creep.memory.quantity);
                     if (result == 0){
                         _.remove(Game.spawns[creep.memory.city].memory.ferryInfo.factoryInfo.transfer,
-                                 _.indexOf([Game.spawns[creep.memory.city].memory.ferryInfo.factoryInfo.transfer, creep.memory.mineral); //remove element
+                                 _.indexOf(Game.spawns[creep.memory.city].memory.ferryInfo.factoryInfo.transfer, creep.memory.mineral)); //remove element
                         creep.say('getJob')
                         break;
                     }
