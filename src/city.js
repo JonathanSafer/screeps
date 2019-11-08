314abb6cbc6a1f2b1ca9ba420f753b9b6f5b1399
Jonathan Safer <jonathansafer2@gmail.com>
2019-11-08 12:21:52 -0500

    removed deprecated method

--- src/city.js
+++ src/city.js
@@ -486,7 +486,7 @@ function updateUpgrader(city, controller, memory, rcl8, creeps) {
         var banks = u.getWithdrawLocations(creeps[0]);
         //console.log(banks);
         var money = _.sum(_.map(banks, bank => bank.store[RESOURCE_ENERGY]));
-        var capacity = _.sum(_.map(banks, bank => bank.storeCapacity));
+        var capacity = _.sum(_.map(banks, bank => bank.store.getCapacity()));
         //console.log('money: ' + money + ', ' + (100*money/capacity));
         if(money < (capacity * .32)){
             memory[rU.name] = Math.max(memory[rU.name] - 1, 1); 
