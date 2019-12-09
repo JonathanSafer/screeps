0047099dabaf7756663093ac695c65102598836b
Jonathan Safer <jonathansafer2@gmail.com>
2019-12-09 14:41:25 -0500

    transporter v2
    using FCBP

--- src/transporter.js
+++ src/transporter.js
@@ -9,85 +9,110 @@ var rT = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(Game.time % 200 === 0){
-            creep.memory.targetId = null;
-        }
         if (creep.saying > 0){
             creep.say(creep.saying - 1)
             return;
         }
         var city = creep.memory.city;
         if (creep.carry.energy == 0) {
-            if (creep.memory.location){
-                var bucket = Game.getObjectById(creep.memory.location);
-                if (actions.withdraw(creep, bucket) == ERR_NOT_ENOUGH_RESOURCES){
-                    var targets = u.getWithdrawLocations(creep);
-                    creep.memory.target = u.getNextLocation(creep.memory.target, targets);
-                    if (targets[creep.memory.target]){
-                        creep.memory.location = targets[creep.memory.target].id
-                    }
+            //refill on energy
+            if(rT.refill(creep, city) === 1){
+                //start moving to target
+                let target = rT.findTarget(creep, null)
+                if(!target){
+                    creep.say(20)
+                    return
                 }
-            } else {
-                var targets = u.getWithdrawLocations(creep);
-                var location = targets[creep.memory.target];
-                if (location == undefined) {
-                  location = Game.spawns[city];
-                  creep.memory.noContainers = true;
-                } else {
-                    creep.memory.noContainers = false;
-                }
-                creep.memory.location = location.id;
-                if (actions.withdraw(creep, location) == ERR_NOT_ENOUGH_RESOURCES){
-                    creep.memory.target = u.getNextLocation(creep.memory.target, targets);
+                if(!creep.pos.isNearTo(target.pos)){
+                    creep.moveTo(target, {reusePath: 15, range: 1});
                 }
             }
         } else {
-            if (creep.memory.targetId){
-                let target = Game.getObjectById(creep.memory.targetId)
-                if (target && target.energy < target.energyCapacity){
-                    actions.charge(creep, target)
-                } else {
-                    if(creep.memory.noContainers){
-                       let locations = creep.room.find(FIND_STRUCTURES, {
-                             filter: (structure) => {
-                                return (structure.structureType == STRUCTURE_EXTENSION 
-                                    || structure.structureType == STRUCTURE_TOWER) 
-                                    && structure.energy < structure.energyCapacity;
-                            }
-                        });
-                        if (locations.length > 2){
-                            actions.charge(creep, locations[Number(creep.name) % 3]);
-                        } else if (locations.length) {
-                            actions.charge(creep, locations[0]);
-                        } 
-                    } else {
-                        let locations = creep.room.find(FIND_STRUCTURES, {
-                                filter: (structure) => {
-                                    return (((structure.structureType == STRUCTURE_EXTENSION 
-                                            || structure.structureType == STRUCTURE_SPAWN
-                                            || structure.structureType == STRUCTURE_LAB
-                                            || structure.structureType == STRUCTURE_NUKER)
-                                            && structure.energy < structure.energyCapacity)
-                                            || (structure.structureType == STRUCTURE_POWER_SPAWN && structure.energy < (structure.energyCapacity - 400))
-                                            || (structure.structureType == STRUCTURE_FACTORY && structure.store.getUsedCapacity(RESOURCE_ENERGY) < 10000)
-                                            || (structure.structureType == STRUCTURE_TOWER && structure.energy < (structure.energyCapacity - 400))
-                                    );
-                                }
-                        });
-                        if (locations.length > 4){
-                            actions.charge(creep, locations[Number(creep.name) % 5]);
-                            creep.memory.targetId = locations[Number(creep.name) % 5].id
-                        } else if (locations.length) {
-                            actions.charge(creep, locations[0]);
-                        } else {
-                            creep.say(20);
-                        }
+            //findClosestByRange(deposit locations)
+            let target = rT.findTarget(creep, null)//for now we will call every tick
+            //do we call this every tick or cache result or something in between?
+
+            if(!target){
+                creep.say(20)
+                return
+            }
+
+            let result = actions.charge(creep, target)
+            if(result === 1){//successful deposit
+                //if creep still has energy, start moving to next target
+                if(creep.store[RESOURCE_ENERGY] > EXTENSION_ENERGY_CAPACITY[creep.room.controller.level]){
+                    //start moving to next target if target not already in range
+                    newTarget = rT.findTarget(creep, target)//make sure to remove current target from search list
+                    if(!newTarget){
+                        creep.say(creep.saying - 1)
+                        return;
                     }
+                    if(!newTarget.pos.isNearTo(creep.pos)){
+                        creep.moveTo(newTarget, {reusePath: 15, range: 1});
+                    }
+                } else {
+                    //start moving to storage already
+                    rT.refill(creep, city)
                 }
-            } else {
-                creep.memory.targetId = 'new';
+
             }
         }
+    },
+ 
+    findTarget: function(creep, oldTarget){
+        let id = 0;
+        if(oldTarget){
+            id = oldTarget.id
+        }
+        let locations = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.id !== id && 
+                        (((structure.structureType == STRUCTURE_EXTENSION 
+                            || structure.structureType == STRUCTURE_SPAWN
+                            || structure.structureType == STRUCTURE_LAB
+                            || structure.structureType == STRUCTURE_NUKER)
+                            && structure.energy < structure.energyCapacity)
+                            || (structure.structureType == STRUCTURE_POWER_SPAWN && structure.energy < (structure.energyCapacity - 400))
+                            || (structure.structureType == STRUCTURE_FACTORY && structure.store.getUsedCapacity(RESOURCE_ENERGY) < 10000)
+                            || (structure.structureType == STRUCTURE_TOWER && structure.energy < (structure.energyCapacity - 400)))
+                    );
+                }
+        });
+        if(locations.length){
+            return creep.pos.findClosestByPath(locations)
+        } else {
+            return false;
+        }
+    },
+
+    refill: function(creep, city){
+        let result = 0;
+        if (creep.memory.location){
+            var bucket = Game.getObjectById(creep.memory.location);
+            result = actions.withdraw(creep, bucket)
+            if (result == ERR_NOT_ENOUGH_RESOURCES){
+                var targets = u.getWithdrawLocations(creep);
+                creep.memory.target = u.getNextLocation(creep.memory.target, targets);
+                if (targets[creep.memory.target]){
+                    creep.memory.location = targets[creep.memory.target].id
+                }
+            }
+        } else {
+            var targets = u.getWithdrawLocations(creep);
+            var location = targets[creep.memory.target];
+            if (location == undefined) {
+              location = Game.spawns[city];
+              creep.memory.noContainers = true;
+            } else {
+                creep.memory.noContainers = false;
+            }
+            creep.memory.location = location.id;
+            result = actions.withdraw(creep, location)
+            if (result == ERR_NOT_ENOUGH_RESOURCES){
+                creep.memory.target = u.getNextLocation(creep.memory.target, targets);
+            }
+        }
+        return result;
     }
 };
 module.exports = rT;
