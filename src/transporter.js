a7606e8e858db96cf7c61243cd5a2742d78cd0cc
Jonathan Safer <jonathansafer2@gmail.com>
2019-12-10 14:48:50 -0500

    transporters in E9N10 beta testing v2.1

--- src/transporter.js
+++ src/transporter.js
@@ -9,11 +9,62 @@ var rT = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        var city = creep.memory.city;
+        if(city == "E9N10"){//new transporter only in this room fort now so we can profile and compare
+            if (creep.saying > 0){
+                creep.say(creep.saying - 1)
+                return;
+            }
+            if(!creep.memory.targetId){
+                creep.memory.targetId = null;
+            }
+            if (creep.carry.energy == 0) {
+                //refill on energy
+                if(rT.refill(creep, city) === 1){
+                    //start moving to target
+                    let target = Game.getObjectByID(creep.memory.targetId)
+                    if(!target || !rT.needsEnergy(target)){//if no target or current target doesn't need energy, try to get a new target
+                        target = rT.getNextTarget(creep)
+                    }
+                    if(!target){//if no targets go dormant
+                        creep.say(20)
+                        return
+                    }
+                    if(!creep.pos.isNearTo(target.pos)){//start moving to target if not already in range
+                        creep.moveTo(target, {reusePath: 15, range: 1});
+                    }
+                }
+            } else {
+                let target = Game.getObjectByID(creep.memory.targetId)
+                if(!target || !rT.needsEnergy(target)){//if no target or current target doesn't need energy, try to get a new target
+                    target = rT.getNextTarget(creep)
+                }
+                if(!target){//if no targets go dormant
+                    creep.say(20)
+                    return
+                }
+                if(actions.charge(creep, target) === 1){//if deposit successful, start moving to next location
+                    if(creep.store[RESOURCE_ENERGY] > target.store.getFreeCapacity(RESOURCE_ENERGY)){
+                        //if energy left over, go to next extension
+                        target = rT.getNextTarget(creep)
+                        if(!target){
+                            creep.say(20)
+                            return;
+                        }
+                        if(!creep.pos.isNearTo(target.pos)){
+                            creep.moveTo(target, {reusePath: 15, range: 1});
+                        }
+                    } else{//go to storage
+                        rT.refill(creep, city)
+                    }
+                }
+            }
+            return
+        }
         if (creep.saying > 0){
             creep.say(creep.saying - 1)
             return;
         }
-        var city = creep.memory.city;
         if (creep.carry.energy == 0) {
             //refill on energy
             if(rT.refill(creep, city) === 1){
@@ -83,6 +134,67 @@ var rT = {
         } else {
             return false;
         }
+    },
+
+    needsEnergy: function(structure){
+        switch(structure.structureType){
+            case STRUCTURE_EXTENSION:
+            case STRUCTURE_SPAWN:
+            case STRUCTURE_LAB:
+            case STRUCTURE_NUKER:
+                //if there is any room for energy, needs energy
+                if(structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0){
+                    return true;
+                } else {
+                    return false;
+                }
+            case STRUCTURE_TOWER:
+                //arbitrary buffer
+                if(structure.store.getFreeCapacity(RESOURCE_ENERGY) > 300){
+                    return true;
+                } else {
+                    return false;
+                }
+            case STRUCTURE_POWER_SPAWN:
+                //arbitrary buffer
+                if(structure.store.getFreeCapacity(RESOURCE_ENERGY) > 1000){
+                    return true;
+                } else {
+                    return false;
+                }
+            case STRUCTURE_FACTORY:
+                //arbitrary max value
+                if(structure.store.getUsedCapacity(RESOURCE_ENERGY) < 10000){
+                    return true;
+                } else {
+                    return false;
+                }
+        }
+    },
+
+    getNextTarget: function(creep){
+        let target = null;
+        let memory = Game.spawns[creep.memory.city].memory
+        if(!creep.memory.target){
+            creep.memory.target = 0;
+        }
+        if(!memory.extensions){
+            return null;
+        }
+        for(var i = creep.memory.target; i < memory.extensions.length; i++){//look at each element starting at memory point
+            //once target is found, save (i + 1) so creep knows not to recheck location it is already depositing at
+            target = Game.getObjectByID(memory.extensions[i])
+            if(needsEnergy(target)){
+                //target found
+                creep.memory.targetId = target.id
+                creep.memory.target = i + 1
+                return target
+            }
+        }
+        //if list completed without finding a target, reset to 0, and return null
+        creep.memory.target = 0
+        creep.memory.targetId = null;
+        return null
     },
 
     refill: function(creep, city){
