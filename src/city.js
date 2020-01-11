62ae626154dfb6991fe1b0aa8f223683886d37f9
jordansafer <jordansafer2@gmail.com>
2020-01-11 09:20:11 -0500

    fix flag check

--- src/city.js
+++ src/city.js
@@ -372,7 +372,7 @@ function updateColonizers(city, memory, closestRoom) {
         if(Game.flags.claim){
             if(Game.spawns[city].room.controller.level < 7){
                 memory[rSB.name] = 4;
-            } else if(Game.flags.claim.room && Game.flags.room.controller && Game.flags.claim.room.controller.level > 6) {
+            } else if(Game.flags.claim.room && Game.flags.claim.room.controller && Game.flags.claim.room.controller.level > 6) {
                 memory[rSB.name] = 4;
             } else {
                 memory[rSB.name] = 2;
