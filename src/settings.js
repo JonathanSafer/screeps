1b5a15b20b1f8471a18d46b8678bddf627906bfb
Jonathan Safer <jonathansafer2@gmail.com>
2020-01-27 14:27:50 -0500

    bug fix

--- src/settings.js
+++ src/settings.js
@@ -4,8 +4,8 @@ var settings = {
     roomplanTime: 500,
     roomplanOffset: 155,
     creditMin: 4500000, //min credits needed to start buying energy
-    miningDisabled = ['W1N210', 'W2N240'], //cities that will attempt any highway mining
-    bucket = {//minimum bucket thresholds
+    miningDisabled: ['W1N210', 'W2N240'], //cities that will attempt any highway mining
+    bucket: {//minimum bucket thresholds
     	mining: 6000 //highway mining
     }
 };
