c423cdc01775dcbbdcb3da5d7367677d981ab3d2
Jonathan Safer <jonathansafer2@gmail.com>
2020-01-28 19:23:29 -0500

    new defender architecture

--- src/defender.js
+++ src/defender.js
@@ -1,5 +1,13 @@
 var a = require('actions');
 var u = require('utils');
+var CreepState = {
+    START: 1,
+    BOOST: 2,
+    ENGAGE: 3,
+    RETREAT: 4,
+    DORMANT: 5,
+};
+var CS = CreepState;
 
 var rD = {
     name: "defender",
@@ -7,8 +15,40 @@ var rD = {
     target: () => 0,
    
 
+
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function(creep) {//
+        const city = creep.memory.city
+        if(Game.spawns[city].room.controller.level == 8){
+            //rcl8 runs new experimental defender
+            //creep is mostly RA, with a tiny bit of heal.
+            //boosted variant has one tough part as well
+            //defenders only made during defcon 2, # of defenders made based on number of enemies
+            if (!creep.memory.state) {
+                creep.memory.state = CS.START
+            }
+            switch (creep.memory.state) {
+                case CS.START:
+                    rD.init(creep)
+                    break
+                case CS.BOOST:
+                    rD.boost(creep)
+                    break
+                case CS.ENGAGE:
+                    rD.engage(creep)
+                    break
+                case CS.RETREAT:
+                    rD.retreat(creep)
+                    break
+                case CS.DORMANT:
+                    rD.dormant(creep)
+                    break
+            }
+
+        }
+
+
+
         if(creep.ticksToLive === 1490) {
             creep.notifyWhenAttacked(false);
         }
@@ -35,6 +75,26 @@ var rD = {
         if (creep.memory.location && !(creep.room.name == creep.memory.location && creep.pos.x > 5 && creep.pos.y > 5 && creep.pos.x < 45 && creep.pos.y < 45)){
             creep.moveTo(new RoomPosition(25, 25, creep.memory.location), {reusePath: 10});
         }
+    },
+
+    init: function(creep){
+        //initialization
+    },
+
+    boost: function(creep){
+        //get boosted, may get boosted using same method as offensive creeps
+    },
+
+    engage: function(creep){
+        //attack designated weak target, or nearest target if no designation
+    },
+
+    retreat: function(creep){
+        //move towards spawn
+    },
+
+    dormant: function(creep){
+        //if in a safe space, hibernate until towers active
     },
     
     iOwn: function(roomName) {
