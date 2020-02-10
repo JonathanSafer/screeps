7244d70dbc1ebdb6d6fdfdd4f53ad68ed0bca68e
jordansafer <jordansafer2@gmail.com>
2020-02-09 22:47:54 -0500

    update with filenames

--- src/profiler-prep.js
+++ src/profiler-prep.js
@@ -2,12 +2,40 @@ var profiler = require("./screeps-profiler")
 
 var p = {
     prepProfile: function() {
-        const fileNames = ["city", "powerCreep", "utils", "markets", "medic",
-            "depositMiner", "roomplan", "bigMedic", "trooper", "bigTrooper",
-            "bigBreaker", "harasser", "spawnBuilder", "claimer", "robber",
-            "ferry", "mineralMiner", "upgrader", "builder", "runner", "breaker",
-            "transporter", "remoteMiner", "defender", "types", "tower",
-            "powerMiner", "labs", "factory"]
+        const fileNames = [
+            "commodityManager",
+            "defender",
+            "depositMiner",
+            "error",
+            "factory",
+            "ferry",
+            "harasser",
+            "labs",
+            "link",
+            "markets",
+            "medic",
+            "mineralMiner",
+            "powerCreep",
+            "powerMiner",
+            "remoteMiner",
+            "robber",
+            "roles",
+            "roomplan",
+            "runner",
+            "screeps-profiler",
+            "settings",
+            "spawnBuilder",
+            "spawnQueue",
+            "stats",
+            "template",
+            "tower",
+            "transporter",
+            "trooper",
+            "types",
+            "unclaimer",
+            "upgrader",
+            "utils",
+        ]
         for (const fileName of fileNames) {
             var lib = require(fileName)
             profiler.registerObject(lib, fileName)
