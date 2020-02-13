68bee1315ac4c28035bd2f32430969c1ea9359b2
Jonathan Safer <jonathansafer2@gmail.com>
2020-02-12 19:03:56 -0500

    indentation

--- src/MemHack.js
+++ src/MemHack.js
@@ -3,20 +3,20 @@
 // At top of loop(): MemHack.pretick()
 // Thats it!
 const MemHack = {
-  memory: null,
-  parseTime: -1,
-  register () {
-    const start = Game.cpu.getUsed()
-    this.memory = Memory
-    const end = Game.cpu.getUsed()
-    this.parseTime = end - start
-    this.memory = RawMemory._parsed
-  },
-  pretick () {
-    delete global.Memory
-    global.Memory = this.memory
-    RawMemory._parsed = this.memory
-  }
+    memory: null,
+    parseTime: -1,
+    register () {
+        const start = Game.cpu.getUsed()
+        this.memory = Memory
+        const end = Game.cpu.getUsed()
+        this.parseTime = end - start
+        this.memory = RawMemory._parsed
+    },
+    pretick () {
+        delete global.Memory
+        global.Memory = this.memory
+        RawMemory._parsed = this.memory
+    }
 }
 MemHack.register()
 module.exports = MemHack
