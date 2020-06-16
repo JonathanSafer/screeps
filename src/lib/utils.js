2709e314bcc8ec19809c966a89ecafbcfbf5ed6e
Jonathan Safer <jonathansafer2@gmail.com>
2020-06-16 13:48:10 -0400

    boosts avail uses new boost lists

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -396,7 +396,7 @@ var u = {
     cacheBoostsAvailable: function(cities) {
         const empireStore = u.empireStore(cities)
         const cityCount = _.filter(cities, city => city.controller.level >= 7).length
-        const boosts = settings.boosts
+        const boosts = settings.civBoosts.concat(settings.militaryBoosts)
         const boostQuantityRequired = settings.boostsNeeded * cityCount
         const boostsAvailable = _(boosts)
             .filter(boost => empireStore[boost] >= boostQuantityRequired)
