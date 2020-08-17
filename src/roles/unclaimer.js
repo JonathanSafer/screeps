2299b1cb7891acb1b87dc084fe4ebcbe8499cf91
Jonathan Safer <jonathansafer2@gmail.com>
2020-08-17 15:10:38 -0400

    unclaimer doesn't need successful unclaim to remove flag

--- src/roles/unclaimer.js
+++ src/roles/unclaimer.js
@@ -12,11 +12,11 @@ var rUC = {
 
     unclaim: function(creep) {
         const result = creep.attackController(creep.room.controller)
+        if(!creep.room.controller.level){
+            delete Memory.flags.unclaim
+        }
         if(result === OK){
             Game.spawns[creep.memory.city].memory[rUC.name] = 0
-            if(!creep.room.controller.level || creep.room.controller.level == 1){
-                delete Memory.flags.unclaim
-            }
             creep.suicide()
         }
     }    
