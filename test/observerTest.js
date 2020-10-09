f1fdef6a0e8da3c2330277a2476c45c15ef51019
Jonathan Safer <jonathansafer2@gmail.com>
2020-10-08 20:54:49 -0400

    update obs test to reflect new range

--- test/observerTest.js
+++ test/observerTest.js
@@ -10,12 +10,12 @@ describe("observer", function () {
     var o = require("../src/buildings/observer.js")
 
     describe("#findRoomsForScan()", function () {
-        it("should find 81 rooms", function () {
+        it("should find 441 rooms", function () {
             const rN = "E0N0"
             const room = new Room(rN)
             new StructureController(room)
             o.findRoomsForScan()
-            assert.equal(81, Cache.rooms[rN].scannerTargets.length)
+            assert.equal(441, Cache.rooms[rN].scannerTargets.length)
         })
     })
 })
