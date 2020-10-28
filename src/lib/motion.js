853df222140cbf196b8dd1938430c3870ee1a622
Jonathan Safer <jonathansafer2@gmail.com>
2020-10-28 12:37:19 -0400

    dangerous rooms still accessible if target

--- src/lib/motion.js
+++ src/lib/motion.js
@@ -229,6 +229,7 @@ var m = {
             roomCallback: function(roomName){
                 const roomData = u.getsetd(roomDataCache, roomName, {})
                 if(roomName != creep.pos.roomName && roomData.owner && !settings.allies.includes(roomData.owner) 
+                    && roomName != goals.roomName
                     && roomData.rcl 
                     && CONTROLLER_STRUCTURES[STRUCTURE_TOWER][roomData.rcl] 
                     && (!creep.memory.tolerance 
