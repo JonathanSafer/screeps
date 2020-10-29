4fc7758910e705ebc49aa0faaf2b8014134b4381
Jonathan Safer <jonathansafer2@gmail.com>
2020-10-29 12:32:01 -0400

    qrCode split into rows

--- src/roles/qrCode.js
+++ src/roles/qrCode.js
@@ -12,12 +12,29 @@ const rQr = {
         if(!flag)
             return
         const localCreeps = u.splitCreepsByCity()[creep.memory.city]
-        while(creep.memory.target < template.qrCoords.length - 2 
-            && !_.find(localCreeps, c => c.memory.role == rQr.name 
-                && c.memory.target == creep.memory.target + 1)){
+        const qrs = _.filter(localCreeps, c => c.memory.role == rQr.name)
+        if(!creep.memory.row === undefined){
+            let freeRow = null
+            for(let i = 0; i < template.qrCoords.length; i++){
+                if(!_.find(qrs, c => c.memory.row == i && c.memory.target == 0)){
+                    freeRow = i
+                    break
+                }
+            }
+            if(freeRow === null){
+                const targetPos = new RoomPosition(Math.max(flag.x - 2,0), Math.max(flag.x - 2,0), flag.roomName)
+                if(!creep.pos.isEqualTo(targetPos))
+                    motion.newMove(creep, targetPos)
+                return
+            }
+            creep.memory.row = freeRow
+        }
+        const row = creep.memory.freeRow
+        while(creep.memory.target < template.qrCoords[row].length - 1 
+            && !_.find(qrs, c => c.memory.row == row && c.memory.target == creep.memory.target + 1)){
             creep.memory.target++
         }
-        const target = template.qrCoords[creep.memory.target]
+        const target = template.qrCoords[row][creep.memory.target]
         const targetPos = new RoomPosition(target.x + flag.x, target.y + flag.y, flag.roomName)
         if(!creep.pos.isEqualTo(targetPos))
             motion.newMove(creep, targetPos)
