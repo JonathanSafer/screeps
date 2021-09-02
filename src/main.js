46e2bbc9265d799b9f2b1f7bc1fef5ce2563ce35
Jonathan Safer <jonathansafer2@gmail.com>
2021-09-02 12:37:46 -0400

    reenabled visuals

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const observer = require("./buildings/observer")
 require("./operations/profiler-prep")
 require("./lib/globals")
 var rr = require("./roles/roles")
+const data = require("./operations/data")
 
 //Code to manually profile:
 //Game.profiler.profile(1000);
@@ -156,6 +157,8 @@ module.exports.loop = function () {
         }
 
         if(Game.time % settings.flagCleanup) u.cleanFlags()
+
+        data.makeVisuals()
 
         // disable emailing
         u.silenceCreeps()
