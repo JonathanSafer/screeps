ca6b5574b931a3a4081ec7ece168f5b865b937ba
Jonathan Safer <jonathansafer2@gmail.com>
2021-09-05 07:50:20 -0400

    data utility functions and framework

--- src/operations/data.js
+++ src/operations/data.js
@@ -7,7 +7,6 @@ sME => safeMode end tick: obs, military
 own => owner: military, obs, roomplan, motion
 rcl => rcl: military, obs, roomplan, visuals, motion
 ctrlP => controller position: obs, roomplan, visuals
-srcP => source positions: obs, roomplan
 src => source IDs: obs, roomplan
 min => mineralType: obs, roomplan
 skL => source keeper lair positions: obs, motion
@@ -24,13 +23,19 @@ NOTE: all positions stored as packed postions. Use utils.unPackPos to get a room
 */
 
 const u = require("../lib/utils")
+const settings = require("../config/settings")
 
 var data = {
     updateData: function(){
         if(!Memory.data){
-            Memory.data = {}
+            Memory.data = {
+                lastReset: 0,
+                uploading: false, //are we uploading and in the middle of a section
+                section: 0, //section being uploaded to. Always recover from other section
+            }
         }
-        
+        data.checkReset()
+        data.recoverData()
 
 
 
@@ -38,9 +43,64 @@ var data = {
 
         //const dataString = JSON.stringify(Cache.roomData)
         //const segmentSize = 50000
-        //load data into  both 1-40 and 41 - 80
+        //150k chars ~= 165kb
+        //373k => 427kb ~4000 rooms
+        //~3 cpu for 87k chars ~100kb, so probably ~30 cpu per tick for backing up data
+        //backup every 1k ticks for 4 ticks => <0.2cpu/tick avg
+        //load data into  both 1-20 and 21 - 40
         //if one side gets corrupted we can recover from the other side
         //otherwise we will update both sides in one 8 tick session
+    },
+
+    checkReset: function () {
+        if(!Cache.time || Cache.time != Game.time - 1){
+            Memory.data.lastReset = Game.time
+        }
+        Cache.time = Game.time
+    },
+
+    recoverData: function() {
+        switch(Game.time - Memory.data.lastReset){
+            case 0:
+                //load first half of data
+                break
+            case 1:
+                //read in first half of data
+                //load second half of data
+                break
+            case 2:
+                //read in second half of data
+                break
+            default:
+                return
+        }
+    },
+
+    backupData: function(){
+        //don't backup during stats update or recovery
+        //backup to section, then toggle section upon completion
+    },
+
+    getBreakPoint: function(str) {
+        let bytes = 0, codePoint, next, i = 0
+
+        while(i < str.length && bytes < 99900){
+            codePoint = str.charCodeAt(i)
+
+            // Lone surrogates cannot be passed to encodeURI
+            if (codePoint >= 0xD800 && codePoint < 0xDC00 && i + 1 < str.length) {
+                next = str.charCodeAt(i + 1)
+                if (next >= 0xDC00 && next < 0xE000) {
+                    bytes += 4
+                    i+= 2
+                    continue
+                }
+            }
+
+            bytes += (codePoint < 0x80 ? 1 : (codePoint < 0x800 ? 2 : 3))
+            i++
+        }
+        return i;
     },
 
     makeVisuals: function(){
