d8bbf64e33303bf2220a1997722c5b4178ce4aa6
jordansafer <jordansafer2@gmail.com>
2022-01-17 15:48:48 -0500

    add room utils

---
+++ src/lib/roomUtils.js
@@ -0,0 +1,114 @@
+var settings = require("../config/settings")
+
+var rU = {
+    isOnEdge: function(pos){//determine if a roomPos is on a room edge
+        return pos.x == 0 || pos.x == 49 || pos.y == 0 || pos.y == 49
+    },
+
+    isNearEdge: function(pos){
+        return pos.x <= 1 || pos.x >= 48 || pos.y <= 1 || pos.y >= 48
+    },
+
+    getWithdrawLocations: function(creep) {
+        var city = creep.memory.city
+        var spawn = Game.spawns[city]
+        var structures = spawn.room.find(FIND_STRUCTURES)
+        return _.filter(structures, structure => structure.structureType == STRUCTURE_CONTAINER ||
+                                                 structure.structureType == STRUCTURE_STORAGE ||
+                                                 structure.structureType == STRUCTURE_TERMINAL)
+    },
+    
+    getTransferLocations: function(creep) {
+        var city = creep.memory.city
+        var spawn = Game.spawns[city]
+        var structures = spawn.room.find(FIND_STRUCTURES)
+        return _.filter(structures, structure => structure.structureType == STRUCTURE_STORAGE ||
+        //mineral miner error when in use                                        structure.structureType == STRUCTURE_SPAWN ||
+                                                structure.structureType == STRUCTURE_CONTAINER)
+    },
+
+    getFactory: function(room) {
+        if (room.controller.level < 7) return false
+
+        // check for existing
+        const roomCache = u.getsetd(Cache, room.name, {})
+        const factory = Game.getObjectById(roomCache.factory)
+        if (factory) return factory
+
+        // look up uncached factory
+        const factories = room.find(FIND_STRUCTURES,{ 
+            filter: { structureType: STRUCTURE_FACTORY } 
+        })
+        if (factories.length) {
+            roomCache.factory = factories[0].id
+            return factories[0]
+        }
+        return false
+    },
+
+    // Get the room's storage location. Priority for storage:
+    // 1. Storage 2. Container 3. Terminal 4. Spawn
+    getStorage: function(room) {
+        // 1. Storage
+        if (room.storage) return room.storage
+        const roomCache = u.getsetd(Cache, room.name, {})
+
+        // 2. Container
+        const container = Game.getObjectById(roomCache.container)
+        if (container) return container  
+        const structures = room.find(FIND_STRUCTURES)
+        const spawn = _.find(structures, struct => struct.structureType == STRUCTURE_SPAWN)
+        const newContainer = spawn && _.find(structures, struct => struct.structureType == STRUCTURE_CONTAINER
+            && struct.pos.inRangeTo(spawn, 3))
+        if (newContainer) {
+            roomCache.container = newContainer.id
+            return newContainer
+        }
+
+        // 3. Terminal
+        if(room.terminal) return room.terminal
+         
+        // 4. Spawn   
+        if (spawn) return spawn
+        return false
+    },
+
+    getAvailableSpawn: function(spawns) {
+        var validSpawns = _.filter(spawns, spawn => !spawn.spawning)
+        if (validSpawns.length > 0) {
+            return validSpawns[0]
+        } else {
+            return null
+        }
+    },
+
+    requestBoosterFill: function(spawn, boosts){
+        if(!spawn.memory.ferryInfo || !spawn.memory.ferryInfo.labInfo){
+            return
+        }
+        const receivers = spawn.memory.ferryInfo.labInfo.receivers
+        for(const mineral of boosts){
+            let receiver = _.find(Object.keys(receivers), lab => receivers[lab].boost == mineral)
+            if(!receiver){
+                receiver = _.find(Object.keys(receivers), lab => !receivers[lab].boost)
+            }
+            if(receiver){
+                receivers[receiver].boost = mineral
+                const lab = Game.getObjectById(receiver)
+                if(lab){
+                    receivers[receiver].fill = Math.ceil((LAB_MINERAL_CAPACITY - (lab.store[mineral] || 0))/1000)
+                }
+            }
+        }
+    },
+
+    isNukeRampart: function(roomPos){
+        const structures = roomPos.lookFor(LOOK_STRUCTURES)
+        if(_.find(structures, struct => settings.nukeStructures.includes(struct.structureType))){
+            return true
+        }
+        return false
+    }
+}
+
+module.exports = rU
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -22,83 +22,11 @@ var u = {
         const labsCache = u.getsetd(Cache, "labs", {})
         return u.getsetd(labsCache, labId, {})
     },
-
-    getWithdrawLocations: function(creep) {
-        var city = creep.memory.city
-        var spawn = Game.spawns[city]
-        var structures = spawn.room.find(FIND_STRUCTURES)
-        return _.filter(structures, structure => structure.structureType == STRUCTURE_CONTAINER ||
-                                                 structure.structureType == STRUCTURE_STORAGE ||
-                                                 structure.structureType == STRUCTURE_TERMINAL)
-    },
-
-    isOnEdge: function(pos){//determine if a roomPos is on a room edge
-        return pos.x == 0 || pos.x == 49 || pos.y == 0 || pos.y == 49
-    },
-
-    isNearEdge: function(pos){
-        return pos.x <= 1 || pos.x >= 48 || pos.y <= 1 || pos.y >= 48
-    },
-    
-    getTransferLocations: function(creep) {
-        var city = creep.memory.city
-        var spawn = Game.spawns[city]
-        var structures = spawn.room.find(FIND_STRUCTURES)
-        return _.filter(structures, structure => structure.structureType == STRUCTURE_STORAGE ||
-        //mineral miner error when in use                                        structure.structureType == STRUCTURE_SPAWN ||
-                                                structure.structureType == STRUCTURE_CONTAINER)
-    },
     
     getNextLocation: function(current, locations) {
         return (current + 1) % locations.length
     },
 
-    getFactory: function(room) {
-        if (room.controller.level < 7) return false
-
-        // check for existing
-        const roomCache = u.getsetd(Cache, room.name, {})
-        const factory = Game.getObjectById(roomCache.factory)
-        if (factory) return factory
-
-        // look up uncached factory
-        const factories = room.find(FIND_STRUCTURES,{ 
-            filter: { structureType: STRUCTURE_FACTORY } 
-        })
-        if (factories.length) {
-            roomCache.factory = factories[0].id
-            return factories[0]
-        }
-        return false
-    },
-
-    // Get the room's storage location. Priority for storage:
-    // 1. Storage 2. Container 3. Terminal 4. Spawn
-    getStorage: function(room) {
-        // 1. Storage
-        if (room.storage) return room.storage
-        const roomCache = u.getsetd(Cache, room.name, {})
-
-        // 2. Container
-        const container = Game.getObjectById(roomCache.container)
-        if (container) return container  
-        const structures = room.find(FIND_STRUCTURES)
-        const spawn = _.find(structures, struct => struct.structureType == STRUCTURE_SPAWN)
-        const newContainer = spawn && _.find(structures, struct => struct.structureType == STRUCTURE_CONTAINER
-            && struct.pos.inRangeTo(spawn, 3))
-        if (newContainer) {
-            roomCache.container = newContainer.id
-            return newContainer
-        }
-
-        // 3. Terminal
-        if(room.terminal) return room.terminal
-         
-        // 4. Spawn   
-        if (spawn) return spawn
-        return false
-    },
-    
     getGoodPickups: function(creep) {
         var city = creep.memory.city
         var localCreeps = u.splitCreepsByCity()
@@ -186,15 +114,6 @@ var u = {
         if(!Tmp.myCities)
             Tmp.myCities = _.filter(Game.rooms, (room) => u.iOwn(room.name))
         return Tmp.myCities
-    },
-
-    getAvailableSpawn: function(spawns) {
-        var validSpawns = _.filter(spawns, spawn => !spawn.spawning)
-        if (validSpawns.length > 0) {
-            return validSpawns[0]
-        } else {
-            return null
-        }
     },
     
     updateCheckpoints: function(creep) {
@@ -361,34 +280,6 @@ var u = {
             }
         }
         return exits
-    },
-
-    requestBoosterFill: function(spawn, boosts){
-        if(!spawn.memory.ferryInfo || !spawn.memory.ferryInfo.labInfo){
-            return
-        }
-        const receivers = spawn.memory.ferryInfo.labInfo.receivers
-        for(const mineral of boosts){
-            let receiver = _.find(Object.keys(receivers), lab => receivers[lab].boost == mineral)
-            if(!receiver){
-                receiver = _.find(Object.keys(receivers), lab => !receivers[lab].boost)
-            }
-            if(receiver){
-                receivers[receiver].boost = mineral
-                const lab = Game.getObjectById(receiver)
-                if(lab){
-                    receivers[receiver].fill = Math.ceil((LAB_MINERAL_CAPACITY - (lab.store[mineral] || 0))/1000)
-                }
-            }
-        }
-    },
-
-    isNukeRampart: function(roomPos){
-        const structures = roomPos.lookFor(LOOK_STRUCTURES)
-        if(_.find(structures, struct => settings.nukeStructures.includes(struct.structureType))){
-            return true
-        }
-        return false
     },
 
     //combine store of all cities given
