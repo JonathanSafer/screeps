aadd100d3fb57c9cfec15e3fc7c4053e861b6912
Jonathan Safer <jonathansafer2@gmail.com>
2022-01-22 14:01:52 -0700

    reserver attacks hostile controller

--- src/roles/reserver.js
+++ src/roles/reserver.js
@@ -1,4 +1,5 @@
 const motion = require("../lib/motion")
+const settings = require("../config/settings")
 
 var rRr = {
     name: "reserver",
@@ -8,7 +9,11 @@ var rRr = {
         const targetRoom = creep.memory.flag
         if(Game.rooms[targetRoom]){
             if(Game.rooms[targetRoom].controller.pos.isNearTo(creep.pos)){
-                creep.reserveController(Game.rooms[targetRoom].controller)
+                if(Game.rooms[targetRoom].controller.reservation && Game.rooms[targetRoom].controller.reservation.username != settings.username){
+                    creep.attackController(Game.rooms[targetRoom].controller)
+                } else {
+                    creep.reserveController(Game.rooms[targetRoom].controller)
+                }
             } else{
                 motion.newMove(creep, Game.rooms[targetRoom].controller, 1)
             }
