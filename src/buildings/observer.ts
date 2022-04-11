9fc3bcfb9367451e2e0fcc04e0677976ae11b328
Jonathan Safer <jonathansafer2@gmail.com>
2022-04-11 13:44:36 -0400

    corrected redundant call

--- src/buildings/observer.ts
+++ src/buildings/observer.ts
@@ -31,7 +31,7 @@ const ob = {
         const roomDataCache = u.getsetd(Cache, "roomData", {})
         for(const room in Game.rooms){
             const roomData = u.getsetd(roomDataCache, room, {})
-            if(!roomData[room].sct || roomData[room].sct < Game.time){
+            if(!roomData.sct || roomData.sct < Game.time){
                 ob.recordData(room, roomData)
                 delete roomsToScan[room]
             }
