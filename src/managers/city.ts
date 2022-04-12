bcad8139a0af765d4e79dc9eded8c08cf9f85a2e
Jonathan Safer <jonathansafer2@gmail.com>
2022-04-12 01:57:06 -0400

    send roomname not object

--- src/managers/city.ts
+++ src/managers/city.ts
@@ -821,7 +821,7 @@ function updateRemotes(city: string){
         const worstRemote = rp.findWorstRemote(spawn.room)
         if(worstRemote){
             Log.info(`Remote ${worstRemote.roomName} removed from ${spawn.room.name}`)
-            rp.removeRemote(worstRemote, spawn.room.name)
+            rp.removeRemote(worstRemote.roomName, spawn.room.name)
         } else {
             Log.info("No remotes to remove")
         }
