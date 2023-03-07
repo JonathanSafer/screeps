fa7230bc7009c62d37e172f698364138913b64b7
Jonathan Safer <jonathansafer2@gmail.com>
2023-03-07 10:42:38 -0500

    re-enabled excess upgrading

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -25,7 +25,7 @@ const settings = {
     upgradeBoostPrice: 500,
     powerBuyVolume: 5000, // amount of power we will buy at once
     processPower: false, //process power instead of selling it
-    rcl8upgrade: false, //use excess energy to GCL pump at RCL8
+    rcl8upgrade: true, //use excess energy to GCL pump at RCL8
 
     miningDisabled: ["W31S41"], //cities that will not attempt any highway mining
     ghodiumAmount: 7000, //threshold to stop producing ghodium
